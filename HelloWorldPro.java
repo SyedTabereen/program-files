5f04121a6e527060d19d62c21e171302ae876a3f
SyedTabereen <51641113+SyedTabereen@users.noreply.github.com>
2019-07-14 01:06:06 +0530

    Add files via upload

---
+++ HelloWorldPro.java
@@ -0,0 +1,10 @@
+// PRPGRAM TO PRINT "hello world"
+package practice;
+public class HelloWorldPro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+	}
+
+}
