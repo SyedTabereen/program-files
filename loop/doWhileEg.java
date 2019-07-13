ddb46713998ce13e536197124b9b9364880da309
SyedTabereen <51641113+SyedTabereen@users.noreply.github.com>
2019-07-14 00:15:29 +0530

    Add files via upload

---
+++ loop/doWhileEg.java
@@ -0,0 +1,20 @@
+package practice;
+
+public class doWhileEg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	char x ='a';
+
+			      do {
+			    	  System.out.println("content of x : " + x );
+			          x++;
+			          }
+			      while( x < 115 );
+
+			}
+
+		}
+	
+
+
